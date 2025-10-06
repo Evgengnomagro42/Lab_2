6c7b5a2a8a282ed7eeb282db1b97c78adc6da084
Evgengnomagro42 <gnom610004@list.ru>
2025-10-06 15:45:28 +0300

    Update lab_2_task_1.cpp

--- lab_2_task_1.cpp
+++ lab_2_task_1.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 float BrokaForMan(float rost) {
     float ideal_ves = (((rost*100)-100)*0.9);
-    cout << "��⨬���� ��� - ��㫠 �ப�, ��: " <<
+    cout << "Оптимальный вес - формула Брока, кг: " <<
     ideal_ves << endl;
     return 0;
   }
 
 float BrokaForWoman(float rost) {
     float ideal_ves = (((rost*100)-100)*0.85);
-    cout << "��⨬���� ��� - ��㫠 �ப�, ��: " <<
+    cout << "Оптимальный вес - формула Брока, кг: " <<
     ideal_ves << endl;
     return 0;
   }
@@ -24,88 +24,88 @@ float BrokaForWoman(float rost) {
 float IMT(float ves, float rost) {
 float index = ves/pow(rost, 2);
 if (index <16.0) {
-  cout << "��ࠦ���� ����� �����, \n"
-  "��� �뭥譨� ���: " << index << endl;
+  cout << "Выраженный дефицит массы, \n"
+  "Ваш нынешний ИМТ: " << index << endl;
 } else if (index>16.0 && index<18.5) {
-cout << "�������筠� (�����) ���� ⥫�, \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Недостаточная (дефицит) масса тела, \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (index>18.5 && index<25.0) {
-cout << "��ଠ. ��� �뭥譨� ���: " << index << endl;
+cout << "Норма. Ваш нынешний ИМТ: " << index << endl;
 } else if (index>25 && index<30) {
-cout << "�����筠� ���� ⥫� (�।���७��) \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Избыточная масса тела (предожирение) \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (index>30 && index<35) {
-cout << "���७�� ��ࢮ� �⥯��� \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Ожирение первой степени \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (index>35 && index<40) {
-cout << "���७�� ��ன �⥯��� \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Ожирение второй степени \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (index>40) {
-cout << "���७�� ���쥩 �⥯��� (��ࡨ����) \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Ожирение третьей степени (морбидное) \n"
+"Ваш нынешний ИМТ: " << index << endl;
   }
 return 0;
 }
 
 float IMTAgeMan (float age, float index){
 if (age >19 && age<=25) {
-  cout << "��ଠ ��� = 21,4 \n"
-  "��� �뭥譨� ���: " << index << endl;
+  cout << "Норма ИМТ = 21,4 \n"
+  "Ваш нынешний ИМТ: " << index << endl;
 } else if (age >25 && age<=35) {
-cout << "��ଠ ��� = 21,6 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 21,6 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >35 && age<=45) {
-cout << "��ଠ ��� = 22,9 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 22,9 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >45 && age<=55) {
-cout << "��ଠ ��� = 25,8 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 25,8 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >55) {
-cout << "��ଠ ��� = 26,6 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 26,6 \n"
+"Ваш нынешний ИМТ: " << index << endl;
  }
 return 0;
 }
 
 float IMTAgeWoman (float age, float index){
 if (age >19 && age<=25) {
-cout << "��ଠ ��� = 19,5 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 19,5 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >25 && age<=35) {
-cout << "��ଠ ��� = 23,2 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 23,2 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >35 && age<=45) {
-cout << "��ଠ ��� = 23,4 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 23,4 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >45 && age<=55) {
-cout << "��ଠ ��� = 25,2 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 25,2 \n"
+"Ваш нынешний ИМТ: " << index << endl;
 } else if (age >55) {
-cout << "��ଠ ��� = 27,3 \n"
-"��� �뭥譨� ���: " << index << endl;
+cout << "Норма ИМТ = 27,3 \n"
+"Ваш нынешний ИМТ: " << index << endl;
     }
   return 0;
   }
 
   void calc_vesa() {
-  cout << "�������� ��� \n";
-  cout << "������ ������, ������ ���, 楫�� �᫮: \n";
+  cout << "Калькулятор веса \n";
+  cout << "Введите возраст, полных лет, целое число: \n";
   float age;
   cin >> age;
-  cout << "������ ���, ��: \n";
+  cout << "Введите вес, кг: \n";
   float ves;
   cin >> ves;
-  cout << "������ ��� � ����� (�,�): \n";
+  cout << "Введите рост в метрах (м,см): \n";
   float rost ;
   cin >> rost;
-  cout << "�᫨ ��稭�, ������ 1, ���騭� - 0 \n";
+  cout << "Если мужчина, введите 1, женщина - 0 \n";
   float pol;
   cin >> pol;
-  cout << "�롥�� ��⮤ � ������ ��� ���:\n\
-  ��㫠 �ப� - ������ 2, \n\
-  ������ ����� ⥫� (���) ��� � � � - ������ 3, \n\
-  ��� ��� � � � � ���⮬ ������ - ������ 4, \n\
-  �᫨ ��� 㢨���� १����� ��� ��⮤�� - ������ 5\n";
+  cout << "Выберите метод и введите его код:\n\
+  формула Брока - введите 2, \n\
+  индекс массы тела (ИМТ) для М и Ж - введите 3, \n\
+  ИМТ для М и Ж с учётом возраста - введите 4, \n\
+  если хотите увидеть результаты всех методов - введите 5\n";
   float kod;
   cin >> kod;
   if (kod==2){
@@ -114,7 +114,7 @@ cout << "��ଠ ��� = 27,3 \n"
     } else if (pol==0) {
     BrokaForWoman(rost);
     /*float ideal_ves = (((rost*100)-100)*0.85);
-    cout << "��⨬���� ��� - ��㫠 �ப�, ��: " <<
+    cout << "Оптимальный вес - формула Брока, кг: " <<
     ideal_ves << endl;*/
     }
   } else if (kod==3) {
@@ -138,8 +138,8 @@ cout << "��ଠ ��� = 27,3 \n"
     IMTAgeWoman (age, ves/pow(rost, 2));
       }
     }
-  if (cin.fail()) { // �᫨ �।��饥 �����祭�� ��������� ��㤠��,
-    cin.clear(); // � �����頥� cin � '�����' ०�� ࠡ���
-    cin.ignore(32767,'\n'); // � 㤠�塞 ���祭�� �।��饣� ����� �� �室���� ����
+  if (cin.fail()) { // если предыдущее извлечение оказалось неудачным,
+    cin.clear(); // то возвращаем cin в 'обычный' режим работы
+    cin.ignore(32767,'\n'); // и удаляем значения предыдущего ввода из входного буфера
     }
   }
